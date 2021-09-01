b7b9dcfbdee295f9aa8e968819bcfaf81df11a19
vincekd <vincekd@gmail.com>
2021-08-31 20:26:47 -0500

    Update, use word template file

--- create-law-review-source-doc.py
+++ create-law-review-source-doc.py
@@ -1,18 +1,31 @@
-import argparse, os, docx
+import argparse, os
 from pathlib import Path
+from docxtpl import DocxTemplate
 
 parser = argparse.ArgumentParser()
-parser.add_argument("example_doc")
+parser.add_argument("template_doc")
 parser.add_argument("output_path")
-parser.add_argument("fn_range")
+parser.add_argument("--range")
+parser.add_argument("--author")
+parser.add_argument("--checker")
+parser.add_argument("--editor")
 args = parser.parse_args()
 
+print(vars(args))
+name = os.path.basename(args.template_doc)
+[start, stop] = args.range.split("-")
+r = range(int(start), int(stop))
+print(name)
 
-doc = docx.Document(args.example_doc)
-table = doc.tables[0]
-print(table)
+doc = DocxTemplate(args.template_doc)
+context = {
+    'start': int(start),
+    'stop': int(stop),
+    'author': args.author,
+    'checker': args.checker,
+    'editor': args.editor,
+}
+print(context)
+doc.render(context)
+doc.save(os.path.join(args.output_path, name))
 
-
-## TODO: create new docx file with fn_range number of tables
-## TODO: add footnote numbers in first cell of each table
-## TODO: keep one empty table at the end
